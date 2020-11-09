acab0e8c74de2176c127210989a8ee491e4182e9
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2020-11-09 18:44:55 +0000

    optimized for a single pass through the array

--- main.clj
+++ main.clj
@@ -3,40 +3,27 @@
 ;; только элементы, встречающиеся более n-раз.
 
 
-(defn counter [arr m]
-    (let [size (count arr)]
-        (if (= size 0)
-            m
-            (let [e (first arr)]
-                (recur 
-                    (rest arr) 
-                    (assoc m e 
-                        (if (contains? m e) 
-                            (inc (get m e))
-                            1
-                        ) 
-                    ) 
+(defn selector-imp [arr m n]
+    (when-not (empty? arr)
+        (let [k (first arr)]
+            (if (contains? m k)
+                (let [v (get m k)]
+                    (if (> v n)
+                        (recur (rest arr) m n)
+                        (if (= v n)
+                            (cons k (selector-imp (rest arr) (assoc m k (inc v)) n))
+                            (recur (rest arr) (assoc m k (inc v)) n)
+                        )
+                    )
                 )
-            )
-        )
-    )
-)
-
-(defn selector-by [m n]
-    (loop [mm m]
-        (when-not (empty? mm)
-            (let [e (first mm) k (first e) v (last e)]
-                (if (> v n)
-                    (cons k (selector-by (rest mm) n))
-                    (recur (rest mm))
-                )
+                (recur (rest arr) (assoc m k 1) n)
             )
         )
     )
 )
 
 (defn selector [arr n]
-    (selector-by (counter arr {}) n)
+    (selector-imp arr {} n)
 )
 
 (selector '(1 1 2 22 2 5 3 0 9 -3 -3 -3 2 5 9 5) 2)
